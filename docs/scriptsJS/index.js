d2e57bf57e9bae2cf238756fdd072e2a1fae76ce
Miguel Angel Fonseca Bello <fou14.mf@gmail.com>
2023-01-31 20:51:29 -0500

    Agregado la funcionalidad/animacion de mover el punto negro

--- docs/scriptsJS/index.js
+++ docs/scriptsJS/index.js
@@ -1,8 +1,38 @@
-var str = "i love JS!",
-j=0,
-res = [];
+window.onload = function () {
+	//todo el codigo va aqui
+	//  ****  Funciones  ****
+	function mueveCirculo(direccion,circulos) {
+		if (direccion == "Izq") {
+			actualImg--;
+			if(actualImg < 0){
+				actualImg = 0;
+			}
+		}
+		else if(direccion == "Der"){
+			actualImg++;
+			if(actualImg > 2){
+				actualImg = 2
+			}			
+		}
+		circulos[actualImg].className = "fa-solid fa-circle";
 
-while((j=str.indexOf(" ",j+1))>0){
-	res.push(j+1);
+		for (let indice = 0; indice < circulos.length; indice++) {
+				if(indice == actualImg){
+					continue;
+				}
+				circulos[indice].className = "fa-regular fa-circle"; 
+			}	
+	}
+//  *********   Ejecucion Main   ***********
+	let circulos = document.getElementsByClassName("fa-circle");
+	var actualImg = 0;
+	let btnIzq = document.getElementById("arrIzq");
+	let btnDer = document.getElementById("arrDer");
+
+	btnIzq.onclick = function(){
+		mueveCirculo("Izq",circulos);
+	}
+	btnDer.onclick = function(){
+		mueveCirculo("Der",circulos);
+	}	
 }
-console.log(res);
